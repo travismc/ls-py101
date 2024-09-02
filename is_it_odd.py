8519874a27656054c9c19a6a2ddc2041ce67e56c
Travis M <>
2024-09-01 19:24:53 -0700

    Add is_it_odd.py as solution to LS PY101-PY109 Small Problems #1."

---
+++ is_it_odd.py
@@ -0,0 +1,13 @@
+# Write a function that takes one integer
+# argument and returns True when the number's
+# absolute value is odd, False otherwise.
+
+
+def odd(integer):
+    return abs(integer) % 2 == 1
+
+
+# print(odd(13))
+# print(odd(2))
+# print(odd(26))
+# print(odd(7))
