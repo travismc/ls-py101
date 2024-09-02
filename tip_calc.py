f3b91f615996a7d1dcd26096ab75378c1b9396d7
Travis M <>
2024-09-02 12:53:36 -0700

    Add solution to PY101-PY109 Small Problems "Tip Calculator"

---
+++ tip_calc.py
@@ -0,0 +1,20 @@
+# Tip Calculator
+# Create a simple tip calculator. The program should prompt for a bill
+# amount and a tip rate. The program must compute the tip, then print
+# both the tip and the total amount of the bill. You can ignore input
+# validation and assume that the user will enter valid numbers.
+# ExampleCopy Code
+# What is the bill? 200
+# What is the tip percentage? 20
+
+# The tip is $40.00
+# The total is $240.00
+
+
+bill = float(input("What is the bill? "))
+tip_percent = float(input("What is the tip percentage? "))
+tip = bill * (tip_percent / 100)
+total = tip + bill
+
+print(f"The tip is ${tip:.2f}")
+print(f"The total is ${total:.2f}")
