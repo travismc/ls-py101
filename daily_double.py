2fa5d1550cf344219cd94a628815542db30ea2e6
Travis M <>
2024-09-15 16:49:31 -0700

    Add solution to PY101-PY109 Small Problems "ddaaiillyy ddoouubbllee"

---
+++ daily_double.py
@@ -0,0 +1,34 @@
+# ddaaiillyy ddoouubbllee
+# Write a function that takes a string argument 
+# and returns a new string that contains the 
+# value of the original string with all 
+# consecutive duplicate characters collapsed 
+# into a single character.
+
+def crunch(string: str) -> str:
+	crunched_string = []
+	idx = 0
+	
+	while idx < len(string):
+		if idx == len(string) - 1 or string[idx] != string[idx + 1]:
+			crunched_string.append(string[idx])
+		
+		idx += 1
+
+	return ''.join(crunched_string)
+
+# Examples
+# These examples should all print True
+print(crunch('ddaaiillyy ddoouubbllee') == 'daily double')
+print(crunch('4444abcabccba') == '4abcabcba')
+print(crunch('ggggggggggggggg') == 'g')
+print(crunch('abc') == 'abc')
+print(crunch('a') == 'a')
+print(crunch('') == '')
+
+print(crunch('ddaaiillyy ddoouubbllee'))
+print(crunch('4444abcabccba'))
+print(crunch('ggggggggggggggg'))
+print(crunch('abc'))
+print(crunch('a'))
+print(crunch(''))
