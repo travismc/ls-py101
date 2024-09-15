e6821674e5edff7682dc896be79f321b6822e380
Travis M <>
2024-09-15 13:43:43 -0700

    Add solution to PY101-PY109 Small Problems "Always Return Negative"

---
+++ always_negative.py
@@ -0,0 +1,21 @@
+# Always Return Negative
+# Write a function that takes a number as an argument. 
+# If the argument is a positive number, return the 
+# negative of that number. If the argument is a 
+# negative number, return it as-is.
+
+def negative(num: int) -> int:
+	if num < 0:
+		return num
+	else:
+		return (num * -1)
+	
+# cleaner solution
+# def negative(number):
+#     return -abs(number)
+
+
+# Examples
+print(negative(5) == -5)      # True
+print(negative(-3) == -3)     # True
+print(negative(0) == 0)       # True
