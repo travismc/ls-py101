472077a98ae178d2a52108e09e39976a30b64589
Travis M <>
2024-09-17 18:12:44 -0700

    Added solution to PY101-PY109 Small Problems "Strings Strings"

---
+++ stringy.py
@@ -0,0 +1,23 @@
+# Strings Strings
+# Write a function that takes one argument, a 
+# positive integer, and returns a string of 
+# alternating '1's and '0's, always starting with a 
+# '1'. The length of the string should match the 
+# given integer.
+
+def stringy(num: int) -> str:
+	result = ""
+	for idx in range(num):
+		digit = '1' if idx % 2 == 0 else '0'
+		result += digit
+
+	return result
+
+
+
+
+# Examples
+print(stringy(6) == "101010")           # True
+print(stringy(9) == "101010101")        # True
+print(stringy(4) == "1010")             # True
+print(stringy(7) == "1010101")          # True
